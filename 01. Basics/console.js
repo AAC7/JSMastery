8fd8f4ee1bb66955835645bdd0612bb1394e2eba
ADASH <anzar254@gmail.com>
2025-05-31 20:21:34 +0530

    first commit

---
+++ 01. Basics/console.js
@@ -0,0 +1,23 @@
+console.log("hello");
+// log
+
+console.log(console); // console object
+
+console.error("This is error");
+console.warn("This is warning");
+console.table({ name: "AC17", age: "10" });
+
+// ------------comments -------------
+
+// This is single line comment
+
+/* 
+    This is
+    Multi line comment
+*/
+
+/** Unorderd comment
+ * bla
+ * bla
+ * bla
+ */
