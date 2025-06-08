56b73acc1e34b912eec64dcc69c67bc006a7bff5
ADASH <anzar254@gmail.com>
2025-06-08 12:16:34 +0530

    premitives

---
+++ 02. Primitives/03-boolean.js
@@ -0,0 +1,12 @@
+// 1. Create Variable name (isJsProgrammingLanguage) & store true as a value.
+// 2. Create variable name (isJsHard) & store false as a value.
+// 3. Create variable name (favNumb) & store your favorite number inside.
+// 4. Now Add favNumber with the value of undefined.
+
+const isJsProgrammingLanguage = true;
+const isJsHard = false;
+const favNumb = 17;
+
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb + undefined); // NaN --> Not a number
